401b29648f3a0399e0d85d486ac1bf9c75460b0e
Benjamin Sherwood <benjamin.sherwood@dmwgroup.co.uk>
2020-08-10 08:52:48 +0100

    DW-4689 - Create Dockerfile and entrypoint script (#1)

--- Dockerfile
+++ Dockerfile
@@ -1,3 +1,22 @@
-FROM alpine:latest
+# Builds a Docker image using nginx with config from S3
 
-CMD ["whoami"]
+FROM nginx:1.19.1-alpine
+
+# See: https://github.com/Docker-Hub-frolvlad/docker-alpine-python3/pull/13
+ENV PYTHONUNBUFFERED=1
+
+RUN apk update
+RUN apk add --no-cache python3 && \
+    if [ ! -e /usr/bin/python ]; then ln -sf python3 /usr/bin/python ; fi && \
+    \
+    python3 -m ensurepip && \
+    rm -r /usr/lib/python*/ensurepip && \
+    pip3 install --trusted-host pypi.python.org --trusted-host files.pythonhosted.org --trusted-host pypi.org --no-cache --upgrade pip setuptools wheel && \
+    if [ ! -e /usr/bin/pip ]; then ln -s pip3 /usr/bin/pip ; fi
+
+RUN pip install --trusted-host pypi.python.org --trusted-host files.pythonhosted.org --trusted-host pypi.org awscli
+
+COPY entrypoint.sh /sbin/entrypoint.sh
+RUN chmod 0755 /sbin/entrypoint.sh
+
+ENTRYPOINT ["sh", "-c", "/sbin/entrypoint.sh && /docker-entrypoint.sh"]
---
+++ entrypoint.sh
@@ -0,0 +1,53 @@
+#!/bin/sh
+set -e
+echo "INFO: Checking container configuration..."
+if [ -z "${NGINX_CONFIG_S3_BUCKET}" -o -z "${NGINX_CONFIG_S3_KEY}" ]; then
+  echo "ERROR: NGINX_CONFIG_S3_BUCKET and NGINX_CONFIG_S3_KEY environment variables must be provided"
+  exit 1
+fi
+
+S3_URI="s3://${NGINX_CONFIG_S3_BUCKET}/${NGINX_CONFIG_S3_KEY}"
+
+# If either of the AWS credentials variables were provided, validate them
+if [ -n "${AWS_ACCESS_KEY_ID}${AWS_SECRET_ACCESS_KEY}" ]; then
+  if [ -z "${AWS_ACCESS_KEY_ID}" -o -z "${AWS_SECRET_ACCESS_KEY}" ]; then
+    echo "ERROR: You must provide both AWS_ACCESS_KEY_ID and AWS_SECRET_ACCESS_KEY variables if you want to use access key based authentication"
+    exit 1
+  else
+    echo "INFO: Using supplied access key for authentication"
+  fi
+
+  # If either of the ASSUMEROLE variables were provided, validate them and configure a shared credentials fie
+  if [ -n "${AWS_ASSUMEROLE_ACCOUNT}${AWS_ASSUMEROLE_ROLE}" ]; then
+    if [ -z "${AWS_ASSUMEROLE_ACCOUNT}" -o -z "${AWS_ASSUMEROLE_ROLE}" ]; then
+      echo "ERROR: You must provide both the AWS_ASSUMEROLE_ACCOUNT and AWS_ASSUMEROLE_ROLE variables if you want to assume role"
+      exit 1
+    else
+      ASSUME_ROLE="arn:aws:iam::${AWS_ASSUMEROLE_ACCOUNT}:role/${AWS_ASSUMEROLE_ROLE}"
+      echo "INFO: Configuring AWS credentials for assuming role to ${ASSUME_ROLE}..."
+      mkdir ~/.aws
+      cat > ~/.aws/credentials << EOF
+[default]
+aws_access_key_id=${AWS_ACCESS_KEY_ID}
+aws_secret_access_key=${AWS_SECRET_ACCESS_KEY}
+
+[${AWS_ASSUMEROLE_ROLE}]
+role_arn=${ASSUME_ROLE}
+source_profile=default
+EOF
+      PROFILE_OPTION="--profile ${AWS_ASSUMEROLE_ROLE}"
+    fi
+  fi
+  if [ -n "${AWS_SESSION_TOKEN}" ]; then
+    sed -i -e "/aws_secret_access_key/a aws_session_token=${AWS_SESSION_TOKEN}" ~/.aws/credentials
+  fi
+else
+  echo "INFO: Using attached IAM roles/instance profiles to authenticate with S3 as no AWS_ACCESS_KEY_ID or AWS_SECRET_ACCESS_KEY have been provided"
+fi
+
+echo "INFO: Copying file from ${S3_URI} to /etc/nginx/ ..."
+aws ${PROFILE_OPTION} s3 cp ${S3_URI} /etc/nginx/nginx.zip
+unzip /etc/nginx/nginx.zip /etc/nginx/
+
+echo "INFO: Testing nginx config..."
+nginx -t >> /var/log/rev-proxy.log
